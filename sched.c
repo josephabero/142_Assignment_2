41a6e95bdf3b01957cf2c6e1f96f886e52c6f9a9
Joseph Abero <joseph.abero@yahoo.com>
2019-04-18 18:48:20 -0700

    Working progress on FIFO

--- sched.c
+++ sched.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct job
+{
+    int job_id;
+    int arrival_time;
+    int duration;
+    int turnaround_time;
+};
+
+void FIFO_Scheduler(struct job *job_list, int num_of_jobs);
+
 int main()
 {
-	FILE *fp;
+    // 1. GET JOB LIST from JOBS.DAT
+    FILE *fp;
 
-	printf("Opening jobs.dat file.\n");
-	fp = fopen("jobs.dat", "r");
-	if(fp == NULL)
-	{
-		perror("Error while opening file.\n");
-		// exit(EXIT_FAILURE);
-	}
+    printf("Opening jobs.dat file.\n");
+    fp = fopen("jobs.dat", "r");
+    if(fp == NULL)
+    {
+        perror("Error while opening file.\n");
+        // exit(EXIT_FAILURE);
+    }
 
-	char line[1000];
-	int job_id, arrival_time, duration;
-	int n;
+    char line[1000];
+    int job_id, arrival_time, duration;
+    int n, index = 0;
+    struct job job_list[100];
 
-	while(fgets(line, 1000, fp) != NULL)
-	{
-		printf("%s", line);
-		n = sscanf(line, "%i %i %i\n", &job_id, &arrival_time, &duration);
-		if(n > 0)
-		{
-			printf("job_id: %i arrival_time: %i duration: %i\n", job_id, arrival_time, duration);
-		}
-	}
+    while(fgets(line, 1000, fp) != NULL)
+    {
+        n = sscanf(line, "%i %i %i\n", &job_id, &arrival_time, &duration);
+        if(n > 0)
+        {
+            job_list[index].job_id = job_id;
+            job_list[index].arrival_time = arrival_time;
+            job_list[index].duration = duration;
+        }
+        index++;
+    }
 
-	fclose(fp);
-	return 0;
+    // for(int i = 0; i < index; i++)
+    // {
+    //         printf("JOB #%i: job_id: %i arrival_time: %i duration: %i\n", i + 1, job_list[i].job_id, job_list[i].arrival_time, job_list[i].duration);
+    // }
+    FIFO_Scheduler(job_list, index);
+
+
+    fclose(fp);
+    return 0;
+}
+
+void FIFO_Scheduler(struct job *job_list, int num_of_jobs)
+{
+    int timer = 0;
+    printf("FIFO Scheduler\n---------------\n");
+    for(int i = 0; i < num_of_jobs; i++)
+    {
+        // printf("JOB #%i: job_id: %i arrival_time: %i duration: %i\n", i + 1, job_list[i].job_id, job_list[i].arrival_time, job_list[i].duration);
+        
+    }
 }
