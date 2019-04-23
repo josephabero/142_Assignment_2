ef9fdc67b57262fdb9fe825c7aa3b348e2f8d073
Joseph Abero <joseph.abero@yahoo.com>
2019-04-23 00:13:32 -0700

    Added functions to prints results, calculate total and response time

--- sched.c
+++ sched.c
@@ -6,8 +6,10 @@ struct job
     int job_id;
     int arrival_time;
     int duration;
+    int start_time;
     int finish_time;
-    int turnaround_time;
+    int total_time;
+    int response_time;
     int run_progress;
 };
 
@@ -19,6 +21,11 @@ void BJF_Scheduler(struct job *job_list, int num_of_jobs);
 void STCF_Scheduler(struct job *job_list, int num_of_jobs);
 void RR_Scheduler(struct job *job_list, int num_of_jobs);
 
+void calculateTotalTime(struct job* running_job);
+void calculateResponseTime(struct job* running_job);
+
+void printSchedulerResults(struct job* resulting_job_list, int num_of_jobs);
+
 int main()
 {
     struct job job_list[100];
@@ -29,8 +36,8 @@ int main()
 
     for(int i = 0; i < num_of_jobs; i++)
     {
-            printf("JOB #%i: job_id: %i arrival_time: %i duration: %i finish_time: %i turnaround_time: %i\n", 
-                i + 1, job_list[i].job_id, job_list[i].arrival_time, job_list[i].duration, job_list[i].finish_time, job_list[i].turnaround_time);
+            printf("JOB #%i: job_id: %i arrival_time: %i duration: %i\n", 
+                i + 1, job_list[i].job_id, job_list[i].arrival_time, job_list[i].duration);
     }
     FIFO_Scheduler(job_list, num_of_jobs);
     SJF_Scheduler(job_list, num_of_jobs);
@@ -67,8 +74,10 @@ void getJobList(struct job job_list[], int *num_of_jobs)
             job_list[index].job_id = job_id;
             job_list[index].arrival_time = arrival_time;
             job_list[index].duration = duration;
+            job_list[index].start_time = -1;
             job_list[index].finish_time = -1;
-            job_list[index].turnaround_time = -1;
+            job_list[index].total_time = -1;
+            job_list[index].response_time = -1;
             job_list[index].run_progress = 0;
         }
         index++;
@@ -82,27 +91,21 @@ void getJobList(struct job job_list[], int *num_of_jobs)
 void FIFO_Scheduler(struct job *job_list, int num_of_jobs)
 {
     int timer = job_list[0].arrival_time;
-    int avg_turnaround = 0;
     printf("\nFIFO Scheduler\n---------------\n");
 
     for(int i = 0; i < num_of_jobs; i++)
     {
         // printf("JOB #%i: job_id: %i arrival_time: %i duration: %i\n", 
         //     i + 1, job_list[i].job_id, job_list[i].arrival_time, job_list[i].duration);
-        printf("JOB %i IS RUNNING.\n", job_list[i].job_id);
+        // printf("JOB %i IS RUNNING.\n", job_list[i].job_id);
+        job_list[i].start_time = timer;
         timer += job_list[i].duration;
         job_list[i].finish_time = timer;
-        job_list[i].turnaround_time = job_list[i].finish_time - job_list[i].arrival_time;
+        calculateTotalTime(&job_list[i]);
+        calculateResponseTime(&job_list[i]);
     }
 
-    for(int i = 0; i < num_of_jobs; i++)
-    {
-        printf("JOB #%i Turnaround Time: %i\n", job_list[i].job_id, job_list[i].turnaround_time);
-        avg_turnaround += job_list[i].turnaround_time;
-    }
-
-    avg_turnaround /= num_of_jobs;
-    printf("AVG Turnaround Time: %i\n", avg_turnaround);
+    printSchedulerResults(job_list, num_of_jobs);
 }
 
 void SJF_Scheduler(struct job *job_list, int num_of_jobs)
@@ -120,11 +123,38 @@ void BJF_Scheduler(struct job *job_list, int num_of_jobs)
 void STCF_Scheduler(struct job *job_list, int num_of_jobs)
 {
     // insert code
-    printf("\nSTCF Scheduler\n---------------\n");
+    printf("\nSTCF Scheduler\n---------------\n");   
 }
 
 void RR_Scheduler(struct job *job_list, int num_of_jobs)
 {
     // insert code
     printf("\nRR Scheduler\n---------------\n");
+}
+
+void calculateTotalTime(struct job* running_job)
+{
+    running_job->total_time = running_job->finish_time - running_job->arrival_time;
+}
+
+void calculateResponseTime(struct job* running_job)
+{
+    running_job->response_time = running_job->start_time - running_job->arrival_time;
+}
+
+void printSchedulerResults(struct job* resulting_job_list, int num_of_jobs)
+{
+    int avg_total_time = 0, avg_response_time = 0;
+    for(int i = 0; i < num_of_jobs; i++)
+    {
+        printf("JOB #%i Start Time: %i Finish Time: %i Total Time: %i Response Time: %i\n", 
+            resulting_job_list[i].job_id, resulting_job_list[i].start_time, resulting_job_list[i].finish_time, resulting_job_list[i].total_time, resulting_job_list[i].response_time);
+
+        avg_total_time += resulting_job_list[i].total_time;
+        avg_response_time += resulting_job_list[i].response_time;
+    }
+
+    avg_total_time /= num_of_jobs;
+    avg_response_time /= num_of_jobs;
+    printf("AVG Total Time: %i AVG Response Time: %i\n", avg_total_time, avg_response_time);
 }
